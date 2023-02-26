350b42f648427af8af2de5c9f8330621c0beb748
iTzAymen <aymeno190@gmail.com>
2023-02-26 20:16:41 +0100

    added more metrics and data visualization + some helper functions

--- packages/CNN_1.py
+++ packages/CNN_1.py
@@ -10,7 +10,7 @@ class AudioClassifier(nn.Module):
 
         # first conv block
         self.conv1 = nn.Conv2d(input_dim, 8, kernel_size=(
-            5, 5), stride=(2, 2), padding=(2, 2))
+            5, 5), stride=(1, 1), padding=(1, 1))
         self.relu1 = nn.ReLU()
         self.bn1 = nn.BatchNorm2d(8)
         self.conv1.bias.data.zero_()
@@ -18,7 +18,7 @@ class AudioClassifier(nn.Module):
 
         # second conv block
         self.conv2 = nn.Conv2d(8, 16, kernel_size=(
-            3, 3), stride=(2, 2), padding=(1, 1))
+            3, 3), stride=(1, 1), padding=(1, 1))
         self.relu2 = nn.ReLU()
         self.bn2 = nn.BatchNorm2d(16)
         self.conv2.bias.data.zero_()
@@ -26,7 +26,7 @@ class AudioClassifier(nn.Module):
 
         # third conv block
         self.conv3 = nn.Conv2d(16, 32, kernel_size=(
-            3, 3), stride=(2, 2), padding=(1, 1))
+            3, 3), stride=(1, 1), padding=(1, 1))
         self.relu3 = nn.ReLU()
         self.bn3 = nn.BatchNorm2d(32)
         self.conv3.bias.data.zero_()
@@ -34,7 +34,7 @@ class AudioClassifier(nn.Module):
 
         # fourth conv block
         self.conv4 = nn.Conv2d(32, 64, kernel_size=(
-            3, 3), stride=(2, 2), padding=(1, 1))
+            3, 3), stride=(1, 1), padding=(1, 1))
         self.relu4 = nn.ReLU()
         self.bn4 = nn.BatchNorm2d(64)
         conv_layers += [self.conv4, self.relu4, self.bn4]
@@ -42,8 +42,13 @@ class AudioClassifier(nn.Module):
         # drop out of 0.5
         self.dropout = nn.Dropout2d(0.5)
         # linear classifier
-        self.ap = nn.AdaptiveAvgPool2d(output_size=1)
-        self.lin = nn.Linear(in_features=64, out_features=output_dim)
+        self.flatten = nn.Flatten()
+        self.lin1 = nn.Linear(in_features=64, out_features=32)
+        self.relu5 = nn.ReLU()
+        self.lin2 = nn.Linear(in_features=32, out_features=16)
+        self.relu6 = nn.ReLU()
+        self.lin3 = nn.Linear(in_features=16, out_features=output_dim)
+        self.relu7 = nn.ReLU()
 
         # wrap the convolutional blocks
         self.conv = nn.Sequential(*conv_layers)
@@ -52,12 +57,14 @@ class AudioClassifier(nn.Module):
         # Run the convolutional blocks
         x = self.conv(x)
         x = self.dropout(x)
-        # Adaptive pool and flatten for input to linear layer
-        x = self.ap(x)
-        x = x.view(x.shape[0], -1)
+        # flatten for input to linear layer
+        x = self.flatten(x)
 
         # Linear layer
-        x = self.lin(x)
-
+        x = self.lin1(x)
+        x = self.relu5(x)
+        x = self.lin2(x)
+        x = self.relu6(x)
+        x = self.lin3(x)
         # Final output
         return x
